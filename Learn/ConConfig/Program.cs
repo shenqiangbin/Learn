68fbde28321bfc2d0d71d95c99a3bc79b2923714
shenqiangbin <shenqiangbin@163.com>
2017-01-15 21:59:08 +0800

    读取配置文件（json文件）

---
+++ Learn/ConConfig/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConfigLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigCenter configCenter = new ConfigCenter();
+            string doorUrl = configCenter.WebSite.Door;
+            Console.WriteLine(doorUrl);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Learn/ConfigLib/ConfigCenter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConfigLib
+{
+    /// <summary>
+    /// 配置中心
+    /// 默认不是测试模式，且读取的是d盘下的website.json配置文件
+    /// 可以自定义，需将configLib.config.json拷贝到引用configLib.dll的目录下（注意：是引用项目的目录）
+    /// 当是测试模式时，将会寻找fake文件夹下的同名配置文件
+    /// </summary>
+    public class ConfigCenter
+    {
+        private string _baseDir = "d:/";
+        private WebSite _webSite;
+        private Config _config;
+
+        public ConfigCenter()
+        {
+            _config = CreateConfig();
+            if (_config.IsFake)
+                _config.BaseDir = Path.Combine(new[] { _config.BaseDir, "fake/" });
+        }
+
+        private Config CreateConfig()
+        {
+            string path = @"ConfigLib.config.json";
+            if (File.Exists(path))
+                return JsonHelper.DeserializeJsonFromFile<Config>(path);
+            else
+                return new Config { IsFake = false, BaseDir = _baseDir };
+        }
+
+        public WebSite WebSite
+        {
+            get
+            {
+                if (_webSite == null)
+                    _webSite = JsonHelper.DeserializeJsonFromFile<WebSite>(_config.BaseDir + @"website.json");
+                return _webSite;
+            }
+        }
+    }
+
+    public class Config
+    {
+        public bool IsFake { get; set; }
+        public string BaseDir { get; set; }
+    }
+
+    public class WebSite
+    {
+        public string Door { get; set; }
+        public string Clinic { get; set; }
+        public string Supplier { get; set; }
+    }
+}
